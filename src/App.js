9490e3bf6bbb8328794fa7ed55a2318fb8ff92e1
Enoch Jongwe <enojo3@outlook.com>
2023-02-04 14:16:46 +0000

    first comment here

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import Box from "./components/box";
+import { useState, useEffect } from 'react';
+import Login from './components/Login';
+
+
+function App() {
+  const [user, setUser] = useState("");
+  const [photos,setPhotos] = useState([]);
+  const [loggedIn,setLoggedIn] = useState(false);
+
+  useEffect(() => {fetchImages()},[]);
+  
+  const myArray = [{name:"Harry"},{ name:"George"}, {name:"Hermione"},{name:"Mafalda"}];
+  
+  const fetchImages = async () => {
+    const response = await fetch ("https://picsum.photos/v2/list");
+    console.log(response)
+    const data = await response.json();
+    console.log(data);
+    setPhotos(data);
+    console.log(photos);
+    }
+    
+
+  return (
+    <div className="App">
+      <Login setter={setUser}/>
+      <h1>{user} is logged in</h1>
+
+      {user ?
+      photos.map((item,index) => {
+        return (
+          <div>
+            <img src={item.download_url} width="300px" />
+            <h2>{item.author}</h2>
+          </div>
+        )
+      })
+      :
+      <h1>Please Login</h1>
+    }
+      <br></br>
+      
+      {/* && is the equivalent of an IF statement */}
+      {/* ? and : are the equivalent of an IF ELSE statement */}
+      {/* <input onChange = {(event) => setUser(event.target.value)} />
+      {user && <Box name={user} />} */}
+      {/* IF user exists then display the username in the Box component */}
+      {/* {(user == "Harry") ? <Box name="harry logged in" /> : <Box name = "harry not logged in"/>} */}
+      {/* IF the username == Harry THEN display Harry logged in ELSE display Harry not logged in */}
+
+      {/* {myArray.map((item,index) => {return (
+        <div>
+          <Box name={item.name} />
+        </div>
+      )})} */}
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/box.js
@@ -0,0 +1,9 @@
+function Box(parameter) {
+    return (
+        <div>
+            <h1>{parameter.name}</h1>
+        </div>
+    )
+};
+
+export default Box;
